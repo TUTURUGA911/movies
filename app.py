fa9ac2937706deb04b661f41be22182bd1d18db4
Feriardus Orlan Banggur <21102077@ittelkom-pwt.ac.id>
2023-10-11 14:33:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
+import requests
 from os.path import join, dirname
 from dotenv import load_dotenv
 
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
+
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
